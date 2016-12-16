e35d92a288e9ca5d1ad04b224b56208ce34586fd
Joe Linstrum <jlinstrum@NEW-INNOV.COM>
2016-12-16 16:04:29 -0500

    My chart updates...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,26 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { BarchartComponent } from './barchart/barchart.component';
+import { HomeComponent } from './home/home.component';
+import { routing, appRoutingProviders } from './app.routes';
+import { ChartDataService } from './chart-data.service';
+import { ChartTestComponent } from './chart-test/chart-test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarchartComponent,
+    HomeComponent,
+    ChartTestComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
-  providers: [],
+  providers: [appRoutingProviders, ChartDataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
